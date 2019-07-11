d5dfe2ad584774f31342a42424f7019b729ecd85
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-11 11:52:12 -0400

    Update to just prompts

--- sketch.js
+++ sketch.js
@@ -1,28 +1,17 @@
 // Any global variables can be defined up here
-var answer;
 
-/*
-    Code in the setup function will only be run once at the start of the animation
-*/
-function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    answer = round(random(1, 100));
-}
 
 /*
     The draw function is executed once per frame.
 */
 function draw() {
     // Update
-    var guess = prompt("what is your guess?", "type your number here (1- 100)");
-    if (answer < guess) {
-        alert("The number is smaller");
-    } else if (answer > guess) {
-        alert("The number is bigger");
-    } else {
-        alert("You did it!");
-        noLoop();
+    var name = prompt("what is your name?", "type your name here");
+    if (name == "Bill Nye") {
+        alert("Science rules");
     }
+    alert("Nice to meet you " + name);
+    noLoop();
 }
 
 
